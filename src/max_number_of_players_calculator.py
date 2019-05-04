058be8f0f72c7bd4305b2d269cb1820d66ee2680
bwielk <bwielk@gmail.com>
2019-05-04 12:05:56 +0100

    Initial project setup with unit testing works as expected

---
+++ src/max_number_of_players_calculator.py
@@ -0,0 +1,11 @@
+
+class MaxNumberOfPlayersCalculator:
+
+    def __init__(self):
+        self.file_to_read = '../resources/players.txt'
+
+    def add_nums(self, num1, num2):
+        return num1 + num2
+
+    def read_the_path(self):
+        return self.file_to_read
